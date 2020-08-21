a1cc63c3290479a8f897cb56d9c73f886084f84c
Sujoy Datta <sujoydatta.2009@gmail.com>
2020-08-22 00:27:23 +0530

    changes

--- zero_one_knapsack_mem.cpp
+++ zero_one_knapsack_mem.cpp
@@ -1,6 +1,7 @@
-//recursive code maximum profit
+//memoization code
 #include<bits/stdc++.h>
 using namespace std;
+int t[100][1000];
 int max(int a, int b) { return (a > b) ? a : b; }
 int knapsack(int weight[],int val[], int c,int n)
 {  
@@ -8,18 +9,25 @@ int knapsack(int weight[],int val[], int c,int n)
     {
         return 0;
     }
+    if(t[n][c]!=-1)
+    {
+        return t[n][c];
+    }
+
+
     if(weight[n-1]<=c)
     {
-       return max((val[n-1]+ knapsack(weight,val,c-weight[n-1],n-1)), knapsack(weight,val,c, n-1));
+       return t[n][c]= max((val[n-1]+ knapsack(weight,val,c-weight[n-1],n-1)), knapsack(weight,val,c, n-1));
     }
     else if (weight[n-1]>c)
-        return knapsack(weight,val,c,n-1);
+        return t[n][c]=knapsack(weight,val,c,n-1);
    return 0;
 }
 int main()
 {   int val[] = { 60, 100, 120 }; //example cases
     int wt[] = { 10, 20, 30 }; 
     int W = 50; 
+    memset(t,-1,sizeof(t));
     int n = sizeof(val) / sizeof(val[0]); 
     cout << knapsack( wt, val,W, n); 
     return 0;
