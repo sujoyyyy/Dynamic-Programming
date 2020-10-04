cdcce372e55c867bca5974b85f246da601242e9c
Sujoy Datta <sujoydatta.2009@gmail.com>
2020-10-05 00:16:23 +0530

    added codes

---
+++ Minimum number of insertion in a string to make it a palindrome/min.cpp
@@ -0,0 +1,6 @@
+/*Minimum number of insertions to make a string palindrome
+Given a string, find the minimum number of characters to be inserted to form Palindrome string out of given string
+
+Examples:
+ab: Number of insertions required is 1. bab
+aa: Number of insertions required is 0. aa*/
